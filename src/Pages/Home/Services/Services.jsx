6bb47fae3d6ab21b5f188e07a84284d8984ac71c
mati <matee803@gmail.com>
2023-11-11 23:27:20 +0600

    first commit

---
+++ src/Pages/Home/Services/Services.jsx
@@ -0,0 +1,38 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import ServicesCard from "./ServicesCard";
+
+
+const Services = () => {
+    const [services, setServices] = useState([]);
+
+    useEffect(() =>{
+      fetch('services.json')
+      .then(res => res.json())
+      .then(data => setServices(data));   
+    }, [] )
+    
+    return (
+        <div className="mt-4">
+            <div className="text-center">
+                <h3 className="text-2xl text-orange-400 font-bold">Our Services</h3>
+                <h2 className="text-5xl font-bold">Our Services Area</h2>
+                <p className="text-s">the majority have suffered alteration in some form, by injected humour, or randomised 
+                <br />
+                words which do not look even slightly believable. </p>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                {/* <p>services{ services.length }</p> */}
+                {
+                    services.map(service => 
+                    <ServicesCard 
+                    key={service._id}
+                    service={service}
+                    ></ServicesCard>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Services;
